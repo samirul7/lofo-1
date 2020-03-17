20723ca14f5a8958b3519c7249eb436d20d9e6f0
Md Samirul Islam <mdsamirulislam7@gmail.com>
2020-03-18 03:06:53 +0530

    first look ot the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, Switch} from 'react-router-dom'
+
+import Header from './components/header/header.component'
+import HomePage from './pages/homepage/homepage.component'
+import SignInPage from './pages/sign-in/sign-in.component'
+import SignUpPage from './pages/sign-up/sign-up.component'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      isLogin : false
+    }
+  }
+
+  render() {
+    const { isLogin } = this.state;
+    return(
+      <div className='App'>
+        <Header isLogin={this.state.isLogin}/>
+        <Switch>
+        <Route path='/' exact component={HomePage} />
+          <Route path='/sign-in' exact component={isLogin?null:SignInPage} />
+          <Route path='/sign-up' exact component={isLogin?null:SignUpPage} />
+        </Switch>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
